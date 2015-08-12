ab57b7e42504a8f1e2463d01bb3d4e1f65936695
alejandro.fuentes <alejandro.fuentes@6f5771a2-32be-460e-a771-7d6402c15993>
2015-08-12 23:30:12 +0000

    DAOTest

---
+++ trunk/source/dictamen/dictamen-soa/dictamen-soa-persistence/src/test/java/mx/gob/imss/cit/dictamen/persistence/dao/NdtR2DespachoDAOTest.java
@@ -0,0 +1,47 @@
+package mx.gob.imss.cit.dictamen.persistence.dao;
+
+import java.util.List;
+
+import mx.gob.imss.cit.dictamen.model.NdtR2DespachoDO;
+import mx.gob.imss.cit.dictamen.persistence.dao.base.AbstractDBTestUnit;
+import mx.gob.imss.cit.dictamen.persistence.dao.impl.NdtR2DespachoDAOImpl;
+
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test para DAO
+ * 
+ * @author cmarmolejo
+ * 
+ */
+@Local
+public class NdtR2DespachoDAOTest extends AbstractDBTestUnit {
+	private NdtR2DespachoDAO localDAO = new NdtR2DespachoDAOImpl();
+
+	private static final Logger LOG = Logger.getLogger(NdtR2DespachoDAOTest.class);
+
+	/**
+	 * Método para configuración de pruebas unitarias
+	 */
+	@Before
+	public void setUp() {
+		// Llamar la prueba padre para obtener el EntityManager
+		super.setUp();
+		// Llamar los datos de negocio
+		// Conectar el EntityManager al servicio y sus daos
+		connect(localDAO);
+
+	}
+
+	@Test
+	public void testFindAll() {
+		LOG.info("prueba");
+		List<NdtR2DespachoDO> resultList = localDAO.findAll();
+		LOG.info("Lista: " + resultList.size());
+		Assert.assertNotNull(resultList);
+	}
+}
+
