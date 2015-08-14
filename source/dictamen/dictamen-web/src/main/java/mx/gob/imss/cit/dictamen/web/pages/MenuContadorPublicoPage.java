5727201e060d41973a71b9d1316896fff5907cd3
alejandro.fuentes <alejandro.fuentes@6f5771a2-32be-460e-a771-7d6402c15993>
2015-08-14 22:33:37 +0000

    Menu Contador Publico Diapositiva 5.

---
+++ source/dictamen/dictamen-web/src/main/java/mx/gob/imss/cit/dictamen/web/pages/MenuContadorPublicoPage.java
@@ -0,0 +1,41 @@
+package mx.gob.imss.cit.dictamen.web.pages;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import mx.gob.imss.cit.dictamen.web.pages.base.BasePage;
+
+@ManagedBean(name = "menuContadorPublicoPage")
+@SessionScoped
+public class MenuContadorPublicoPage extends BasePage{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	private List<String> images;
+	
+	@PostConstruct
+	public void init() {
+		images = new ArrayList<String>();
+
+		images.add("IMSS_Banner_001.jpg");
+		images.add("IMSS_Banner_002.jpg");
+		images.add("IMSS_Banner_003.jpg");
+		
+	}
+
+	public List<String> getImages() {
+		return images;
+	}
+
+	public void setImages(List<String> images) {
+		this.images = images;
+	}
+}
