997b958a912a808fbf55cb440a87d5b211ff2fe8
dlopezf <dlopezf@6f5771a2-32be-460e-a771-7d6402c15993>
2015-08-19 00:16:29 +0000

    Tranformador de DO a TO

---
+++ source/dictamen/dictamen-soa/dictamen-soa-services/src/main/java/mx/gob/imss/cit/dictamen/services/constants/ServicesConstants.java
@@ -0,0 +1,28 @@
+package mx.gob.imss.cit.dictamen.services.constants;
+
+
+/**
+ * The Class PortalConstants.
+ */
+public final class ServicesConstants {
+
+	/**
+	 * Instantiates a new portal constants.
+	 */
+	private ServicesConstants() {
+	}
+	
+	
+	
+	public static final String SEPARADOR_PARAM_ARCHIVO="#";
+	
+	public static final String SEPARADOR_PARAM_CLAVE="-";
+	
+	public static final int CERO=0;
+	
+	public static final int UNO=1;
+	
+	public static final int PROFUNDIDAD_MAPEO=1;
+	
+	
+}
