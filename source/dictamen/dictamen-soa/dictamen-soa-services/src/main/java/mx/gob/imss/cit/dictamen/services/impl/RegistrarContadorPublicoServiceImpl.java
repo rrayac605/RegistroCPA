c3fddf23066b001a7b12b563fb210c41e324cbcd
lsanchezs <lsanchezs@6f5771a2-32be-460e-a771-7d6402c15993>
2015-08-20 23:46:39 +0000

    + Version Inicial de pantallas de Medio de Contacto

--- source/dictamen/dictamen-soa/dictamen-soa-services/src/main/java/mx/gob/imss/cit/dictamen/services/impl/RegistrarContadorPublicoServiceImpl.java
+++ source/dictamen/dictamen-soa/dictamen-soa-services/src/main/java/mx/gob/imss/cit/dictamen/services/impl/RegistrarContadorPublicoServiceImpl.java
@@ -2,17 +2,23 @@ package mx.gob.imss.cit.dictamen.services.impl;
 
 import javax.ejb.Stateless;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import mx.gob.imss.cit.dictamen.commons.to.ContadorPublicoTO;
 import mx.gob.imss.cit.dictamen.services.RegistrarContadorPublicoService;
 
-
 @Stateless
-public class RegistrarContadorPublicoServiceImpl implements RegistrarContadorPublicoService{
+public class RegistrarContadorPublicoServiceImpl implements
+		RegistrarContadorPublicoService {
+
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(RegistrarContadorPublicoServiceImpl.class);
 
 	@Override
 	public void executeRegistrarContador(ContadorPublicoTO contadorPublicoTO) {
-		// TODO Auto-generated method stub
-		
+		LOGGER.info("###################### El EJB RegistrarContadorPublicoServiceImpl se ejecuto exitosamente ###########################################3");
+
 	}
 
 }
