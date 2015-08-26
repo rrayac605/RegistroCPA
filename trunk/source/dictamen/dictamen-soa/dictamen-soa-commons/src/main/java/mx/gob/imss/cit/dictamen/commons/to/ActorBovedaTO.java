b25db7a5b7607f9ea77eda7f0b3b7d48bde70383
alejandro.fuentes <alejandro.fuentes@6f5771a2-32be-460e-a771-7d6402c15993>
2015-08-26 19:20:00 +0000

    TOs para BovedaWS

---
+++ trunk/source/dictamen/dictamen-soa/dictamen-soa-commons/src/main/java/mx/gob/imss/cit/dictamen/commons/to/ActorBovedaTO.java
@@ -0,0 +1,54 @@
+package mx.gob.imss.cit.dictamen.commons.to;
+
+/**
+ * 
+ * 
+ * @author cmarmolejo
+ * 
+ */
+public class ActorBovedaTO {
+	private String id;
+	private String isOwner;
+	private String org;
+	private String rol;
+	private String tipoId;
+	private String zona;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getIsOwner() {
+		return isOwner;
+	}
+	public void setIsOwner(String isOwner) {
+		this.isOwner = isOwner;
+	}
+	public String getOrg() {
+		return org;
+	}
+	public void setOrg(String org) {
+		this.org = org;
+	}
+	public String getRol() {
+		return rol;
+	}
+	public void setRol(String rol) {
+		this.rol = rol;
+	}
+	public String getTipoId() {
+		return tipoId;
+	}
+	public void setTipoId(String tipoId) {
+		this.tipoId = tipoId;
+	}
+	public String getZona() {
+		return zona;
+	}
+	public void setZona(String zona) {
+		this.zona = zona;
+	}	
+}
+
