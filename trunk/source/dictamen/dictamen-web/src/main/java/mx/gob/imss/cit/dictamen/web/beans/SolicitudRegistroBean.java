b60613f04f893c058efc229ada07ddd84782d01d
lsanchezs <lsanchezs@6f5771a2-32be-460e-a771-7d6402c15993>
2015-08-27 01:21:33 +0000

    + Se homolga estilo de las pantallas

--- trunk/source/dictamen/dictamen-web/src/main/java/mx/gob/imss/cit/dictamen/web/beans/SolicitudRegistroBean.java
+++ trunk/source/dictamen/dictamen-web/src/main/java/mx/gob/imss/cit/dictamen/web/beans/SolicitudRegistroBean.java
@@ -4,23 +4,24 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 
+import org.primefaces.context.RequestContext;
+import org.primefaces.event.FlowEvent;
+
 import mx.gob.imss.cit.dictamen.web.beans.base.BaseBean;
 import mx.gob.imss.cit.dictamen.web.constants.NavigationConstants;
 import mx.gob.imss.cit.dictamen.web.pages.SolicitudRegistroPage;
 
 @ManagedBean(name = "solicitudRegistroBean")
 @ViewScoped
-public class SolicitudRegistroBean extends BaseBean{
+public class SolicitudRegistroBean extends BaseBean {
 
 	private static final long serialVersionUID = 7107693419877419759L;
-	
-	
-	@ManagedProperty(value = "#{solicitudRegistroPage}")
-	private  SolicitudRegistroPage  solicitudRegistroPage;
 
-	public String initSolicitudRegistro(){
-		
-		
+	@ManagedProperty(value = "#{solicitudRegistroPage}")
+	private SolicitudRegistroPage solicitudRegistroPage;
+
+	public String initSolicitudRegistro() {
+
 		return NavigationConstants.PAG_SOLICITUD_REGISTRO;
 	}
 	
@@ -31,6 +32,22 @@ public class SolicitudRegistroBean extends BaseBean{
 	}
 
 	/**
+	 * Metodo que permite controlar la navegacion del componente Wizard de la
+	 * solicitud de registro
+	 * 
+	 * @param flowEvent
+	 * @return
+	 */
+	public String onFlowProcessWizard(FlowEvent flowEvent) {
+		if (flowEvent.getNewStep().equals("mediosContactoTab")) {
+			RequestContext context = RequestContext.getCurrentInstance();
+			context.execute("menuMedioContactoDialog.show();");
+		}
+
+		return flowEvent.getNewStep();
+	}
+
+	/**
 	 * @return the solicitudRegistroPage
 	 */
 	public SolicitudRegistroPage getSolicitudRegistroPage() {
@@ -38,9 +55,11 @@ public class SolicitudRegistroBean extends BaseBean{
 	}
 
 	/**
-	 * @param solicitudRegistroPage the solicitudRegistroPage to set
+	 * @param solicitudRegistroPage
+	 *            the solicitudRegistroPage to set
 	 */
-	public void setSolicitudRegistroPage(SolicitudRegistroPage solicitudRegistroPage) {
+	public void setSolicitudRegistroPage(
+			SolicitudRegistroPage solicitudRegistroPage) {
 		this.solicitudRegistroPage = solicitudRegistroPage;
 	}
 
