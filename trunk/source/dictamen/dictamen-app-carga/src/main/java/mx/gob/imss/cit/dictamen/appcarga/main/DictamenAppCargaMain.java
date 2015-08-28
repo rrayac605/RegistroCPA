bbdb76ffde1e53b4bce829b4f07c331f2b15519d
alejandro.fuentes <alejandro.fuentes@6f5771a2-32be-460e-a771-7d6402c15993>
2015-08-28 00:43:41 +0000

    estrutura proyecto carga de archivos

---
+++ trunk/source/dictamen/dictamen-app-carga/src/main/java/mx/gob/imss/cit/dictamen/appcarga/main/DictamenAppCargaMain.java
@@ -0,0 +1,22 @@
+
+package mx.gob.imss.cit.dictamen.appcarga.main;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author ajfuentes
+ *
+ */
+public class DictamenAppCargaMain {
+
+	private static final Logger LOGGER = Logger.getLogger(DictamenAppCargaMain.class);
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		LOGGER.info("inicio de app carga");
+
+	}
+
+}
