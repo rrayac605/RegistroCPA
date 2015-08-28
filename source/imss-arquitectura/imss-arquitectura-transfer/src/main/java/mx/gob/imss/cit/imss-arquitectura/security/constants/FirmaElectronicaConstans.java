a9f81975ea04f0ec39dd07bc8d41b289174e7694
alejandro.fuentes <alejandro.fuentes@6f5771a2-32be-460e-a771-7d6402c15993>
2015-08-28 18:16:41 +0000

    proyectos de arquitectura reusables

---
+++ source/imss-arquitectura/imss-arquitectura-transfer/src/main/java/mx/gob/imss/cit/imss-arquitectura/security/constants/FirmaElectronicaConstans.java
@@ -0,0 +1,27 @@
+package mx.gob.imss.cit.dictamen.commons.constants;
+
+public class FirmaElectronicaConstans {
+	
+	private FirmaElectronicaConstans(){
+		
+	}
+	
+
+	public static final String FIRMA_ELECTRONICA_FILE_PROPERTIES = "firma_electronica-config";
+	
+	public static final String KEY_URL_APPLET_FIEL="URL_APPLET_FIEL";
+	
+	public static final String KEY_FIEL_ARCHIVE="FIEL_ARCHIVE";
+	
+	public static final String KEY_FIEL_CODE="FIEL_CODE";
+	
+	public static final String KEY_FIEL_CODEBASE="FIEL_CODEBASE";
+	
+	public static final String KEY_FIEL_PARAM_OPTION="FIEL_PARAM_OPTION";
+	
+	public static final String KEY_FIEL_PARAM_URL_WS_FE="FIEL_PARAM_URL_WS_FE";
+	
+	public static final String KEY_FIEL_PARAM_URL_WS_AUTH="FIEL_PARAM_URL_WS_AUTH";
+	
+	
+}
