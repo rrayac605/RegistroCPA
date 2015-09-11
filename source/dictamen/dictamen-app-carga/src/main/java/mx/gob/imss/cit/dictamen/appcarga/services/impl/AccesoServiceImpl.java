ebbb4a6836bd930f3c79d2ff530b094b0fffcac9
alejandro.fuentes <alejandro.fuentes@6f5771a2-32be-460e-a771-7d6402c15993>
2015-09-11 02:21:10 +0000

    se eslimna le proyecto de app-carga

--- source/dictamen/dictamen-app-carga/src/main/java/mx/gob/imss/cit/dictamen/appcarga/services/impl/AccesoServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package mx.gob.imss.cit.dictamen.appcarga.services.impl;
-
-import mx.gob.imss.cit.dictamen.appcarga.services.AccesoService;
-
-public class AccesoServiceImpl implements AccesoService {
-
-}
