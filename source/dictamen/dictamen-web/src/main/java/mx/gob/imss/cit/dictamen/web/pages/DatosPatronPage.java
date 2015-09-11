3707b3630d2b443019f579d6dc1569576df894f3
dlopezf <dlopezf@6f5771a2-32be-460e-a771-7d6402c15993>
2015-09-11 02:35:03 +0000

    se agrega pantalla de carga de archivos

--- source/dictamen/dictamen-web/src/main/java/mx/gob/imss/cit/dictamen/web/pages/DatosPatronPage.java
+++ source/dictamen/dictamen-web/src/main/java/mx/gob/imss/cit/dictamen/web/pages/DatosPatronPage.java
@@ -1,9 +1,12 @@
 package mx.gob.imss.cit.dictamen.web.pages;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import mx.gob.imss.cit.dictamen.integration.api.dto.DatosPatronDTO;
+import mx.gob.imss.cit.dictamen.integration.api.dto.ParentLayoutDTO;
 import mx.gob.imss.cit.dictamen.web.beans.base.BaseBean;
 @ManagedBean(name = "datosPatronPage")
 @ViewScoped
@@ -14,8 +17,9 @@ public class DatosPatronPage extends BaseBean {
 	 */
 	private static final long serialVersionUID = 3595076953499269235L;
 	
-	DatosPatronDTO datosPatron;
+	private DatosPatronDTO datosPatron;
 
+	private List<ParentLayoutDTO> listaParentLayout;
 	/**
 	 * @return the datosPatron
 	 */
@@ -29,7 +33,21 @@ public class DatosPatronPage extends BaseBean {
 	public void setDatosPatron(DatosPatronDTO datosPatron) {
 		this.datosPatron = datosPatron;
 	}
-	
+
+	/**
+	 * @return the listaParentLayout
+	 */
+	public List<ParentLayoutDTO> getListaParentLayout() {
+		return listaParentLayout;
+	}
+
+	/**
+	 * @param listaParentLayout the listaParentLayout to set
+	 */
+	public void setListaParentLayout(List<ParentLayoutDTO> listaParentLayout) {
+		this.listaParentLayout = listaParentLayout;
+	}
+
 	
 	
 }
