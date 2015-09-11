4f092fba8dab705d2bfddfdf86a70bdaeb63c9ba
dlopezf <dlopezf@6f5771a2-32be-460e-a771-7d6402c15993>
2015-09-11 02:35:03 +0000

    se agrega pantalla de carga de archivos

--- trunk/source/dictamen/dictamen-soa/dictamen-soa-integration-api/src/main/java/mx/gob/imss/cit/dictamen/integration/api/dto/ParentLayoutDTO.java
+++ trunk/source/dictamen/dictamen-soa/dictamen-soa-integration-api/src/main/java/mx/gob/imss/cit/dictamen/integration/api/dto/ParentLayoutDTO.java
@@ -1,8 +1,10 @@
 package mx.gob.imss.cit.dictamen.integration.api.dto;
 
+import java.util.List;
+
 import mx.gob.imss.cit.dictamen.integration.api.dto.base.BaseDTO;
 
-public class LayoutDTO extends BaseDTO  {
+public class ParentLayoutDTO extends BaseDTO  {
 
 	/**
 	 * 
@@ -10,19 +12,30 @@ public class LayoutDTO extends BaseDTO  {
 	private static final long serialVersionUID = -2644866008364925551L;
 	
 	private String name;
-
+	private List<LayoutDTO> listaLayout;
 	/**
 	 * @return the name
 	 */
 	public String getName() {
 		return name;
 	}
-
 	/**
 	 * @param name the name to set
 	 */
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	/**
+	 * @return the listaLayout
+	 */
+	public List<LayoutDTO> getListaLayout() {
+		return listaLayout;
+	}
+	/**
+	 * @param listaLayout the listaLayout to set
+	 */
+	public void setListaLayout(List<LayoutDTO> listaLayout) {
+		this.listaLayout = listaLayout;
+	}
+	
 }
--- trunk/source/dictamen/dictamen-web/src/main/java/mx/gob/imss/cit/dictamen/web/beans/DatosPatronBean.java
+++ trunk/source/dictamen/dictamen-web/src/main/java/mx/gob/imss/cit/dictamen/web/beans/DatosPatronBean.java
@@ -1,9 +1,14 @@
 package mx.gob.imss.cit.dictamen.web.beans;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 
+import mx.gob.imss.cit.dictamen.integration.api.dto.LayoutDTO;
+import mx.gob.imss.cit.dictamen.integration.api.dto.ParentLayoutDTO;
 import mx.gob.imss.cit.dictamen.web.beans.base.BaseBean;
 import mx.gob.imss.cit.dictamen.web.enums.MensajesNotificacionesEnum;
 import mx.gob.imss.cit.dictamen.web.pages.DatosPatronPage;
@@ -21,14 +26,16 @@ public class DatosPatronBean extends BaseBean {
 	@ManagedProperty(value = "#{datosPatronPage}")
 	private DatosPatronPage datosPatronPage;
 
-	public void init(){
-
+	public String init(){
+		datosPatronPage.setListaParentLayout(createList());
+		
+		return null;
 	}
 	
 	public void init(String rfc){
 		
 	}
-	
+
 	public void buscar(){
 		FacesUtils.messageSuccess(MensajesNotificacionesEnum.MSG_EXITO_EJEMPLO.getCode());
 		FacesUtils.messageError(MensajesNotificacionesEnum.MSG_ERROR_EJEMPLO2.getCode());
@@ -36,19 +43,83 @@ public class DatosPatronBean extends BaseBean {
 		FacesUtils.messageError(MensajesNotificacionesEnum.MSG_ERROR_EJEMPLO4.getCode()," hola","hola2");
 	}
 
-	/**
-	 * @return the datosPatronPage
-	 */
-	public DatosPatronPage getDatosPatronPage() {
-		return datosPatronPage;
-	}
 
+
+	public List<ParentLayoutDTO> createList(){
+		List<ParentLayoutDTO> listaParentLayout=new ArrayList<ParentLayoutDTO>();
+		
+		ParentLayoutDTO parentLayoutDTO=new ParentLayoutDTO();
+		parentLayoutDTO.setListaLayout(new ArrayList<LayoutDTO>());
+		
+		parentLayoutDTO.setName("Layout Tipo1");
+		LayoutDTO layoutDTO=new LayoutDTO();
+		layoutDTO.setName("Documento 1 de L1");
+		parentLayoutDTO.getListaLayout().add(layoutDTO);
+		layoutDTO=new LayoutDTO();
+		layoutDTO.setName("Documento 2 de L1");
+		parentLayoutDTO.getListaLayout().add(layoutDTO);
+		layoutDTO=new LayoutDTO();
+		layoutDTO.setName("Documento 3 de L1");
+		parentLayoutDTO.getListaLayout().add(layoutDTO);
+		
+		layoutDTO=new LayoutDTO();
+		layoutDTO.setName("Documento 4 de L1");
+		parentLayoutDTO.getListaLayout().add(layoutDTO);
+		layoutDTO=new LayoutDTO();
+		layoutDTO.setName("Documento 5 de L1");
+		parentLayoutDTO.getListaLayout().add(layoutDTO);
+		
+		listaParentLayout.add(parentLayoutDTO);		
+		
+		
+		
+		
+		
+		parentLayoutDTO=new ParentLayoutDTO();
+		parentLayoutDTO.setListaLayout(new ArrayList<LayoutDTO>());
+		
+		parentLayoutDTO.setName("Layout Tipo2");
+		layoutDTO=new LayoutDTO();
+		layoutDTO.setName("Documento 1 de L2");
+		parentLayoutDTO.getListaLayout().add(layoutDTO);
+		layoutDTO=new LayoutDTO();
+		layoutDTO.setName("Documento 2 de L2");
+		parentLayoutDTO.getListaLayout().add(layoutDTO);
+		layoutDTO=new LayoutDTO();
+		layoutDTO.setName("Documento 3 de L3");
+		parentLayoutDTO.getListaLayout().add(layoutDTO);
+		listaParentLayout.add(parentLayoutDTO);
+		
+		
+		
+		parentLayoutDTO=new ParentLayoutDTO();
+		parentLayoutDTO.setListaLayout(new ArrayList<LayoutDTO>());
+		
+		parentLayoutDTO.setName("Layout Tipo3");
+		layoutDTO=new LayoutDTO();
+		layoutDTO.setName("Documento 1 de L3");
+		parentLayoutDTO.getListaLayout().add(layoutDTO);
+		layoutDTO=new LayoutDTO();
+		layoutDTO.setName("Documento 2 de L3");
+		parentLayoutDTO.getListaLayout().add(layoutDTO);
+		layoutDTO=new LayoutDTO();
+		layoutDTO.setName("Documento 3 de L3");
+		parentLayoutDTO.getListaLayout().add(layoutDTO);
+		listaParentLayout.add(parentLayoutDTO);
+		
+		
+		
+		
+		return listaParentLayout;
+	}
+	
 	/**
 	 * @param datosPatronPage the datosPatronPage to set
 	 */
 	public void setDatosPatronPage(DatosPatronPage datosPatronPage) {
 		this.datosPatronPage = datosPatronPage;
 	}
-	
-	
+
+
+
 }
