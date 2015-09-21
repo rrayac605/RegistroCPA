73676441cfe677a0eb81b95a797289ee06edd190
alejandro.fuentes <alejandro.fuentes@6f5771a2-32be-460e-a771-7d6402c15993>
2015-09-21 15:46:57 +0000

    se modifica la perssitencia debido a la base de datos

--- source/dictamen/dictamen-soa/dictamen-soa-commons/src/main/java/mx/gob/imss/cit/dictamen/commons/to/domain/PatronDictamenTO.java
+++ source/dictamen/dictamen-soa/dictamen-soa-commons/src/main/java/mx/gob/imss/cit/dictamen/commons/to/domain/PatronDictamenTO.java
@@ -3,6 +3,8 @@
  */
 package mx.gob.imss.cit.dictamen.commons.to.domain;
 
+import java.util.Date;
+
 import mx.gob.imss.cit.dictamen.commons.to.base.BaseTO;
 
 /**
@@ -14,6 +16,218 @@ public class PatronDictamenTO extends BaseTO{
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;    
+
+    private Long cveIdPatronDictamen;
+    private String desRfc;
+    private String desNombreRazonSocial;
+    private Integer numNumeroTrabajadores;
+    private Integer numRegistroPatronales;
+    private Integer indPatronObligado;
+    private Short indPatronConstruccion;
+    private Short indRealizoActConstruccion;
+    private Short indPatronEmpresaValuada;
+    private String desNumeroFolioDictamen;
+    private Date fecRegistroAlta;
+    private Date fecRegistroBaja;
+    private Date fecRegistroAutorizado;
+    private String cveIdUsuario;
+    private Long cveIdTipoDictamenId;    
+    private Long cveIdEjerFiscalId;
+	/**
+	 * @return the cveIdPatronDictamen
+	 */
+	public Long getCveIdPatronDictamen() {
+		return cveIdPatronDictamen;
+	}
+	/**
+	 * @param cveIdPatronDictamen the cveIdPatronDictamen to set
+	 */
+	public void setCveIdPatronDictamen(Long cveIdPatronDictamen) {
+		this.cveIdPatronDictamen = cveIdPatronDictamen;
+	}
+	/**
+	 * @return the desRfc
+	 */
+	public String getDesRfc() {
+		return desRfc;
+	}
+	/**
+	 * @param desRfc the desRfc to set
+	 */
+	public void setDesRfc(String desRfc) {
+		this.desRfc = desRfc;
+	}
+	/**
+	 * @return the desNombreRazonSocial
+	 */
+	public String getDesNombreRazonSocial() {
+		return desNombreRazonSocial;
+	}
+	/**
+	 * @param desNombreRazonSocial the desNombreRazonSocial to set
+	 */
+	public void setDesNombreRazonSocial(String desNombreRazonSocial) {
+		this.desNombreRazonSocial = desNombreRazonSocial;
+	}
+	/**
+	 * @return the numNumeroTrabajadores
+	 */
+	public Integer getNumNumeroTrabajadores() {
+		return numNumeroTrabajadores;
+	}
+	/**
+	 * @param numNumeroTrabajadores the numNumeroTrabajadores to set
+	 */
+	public void setNumNumeroTrabajadores(Integer numNumeroTrabajadores) {
+		this.numNumeroTrabajadores = numNumeroTrabajadores;
+	}
+	/**
+	 * @return the numRegistroPatronales
+	 */
+	public Integer getNumRegistroPatronales() {
+		return numRegistroPatronales;
+	}
+	/**
+	 * @param numRegistroPatronales the numRegistroPatronales to set
+	 */
+	public void setNumRegistroPatronales(Integer numRegistroPatronales) {
+		this.numRegistroPatronales = numRegistroPatronales;
+	}
+	/**
+	 * @return the indPatronObligado
+	 */
+	public Integer getIndPatronObligado() {
+		return indPatronObligado;
+	}
+	/**
+	 * @param indPatronObligado the indPatronObligado to set
+	 */
+	public void setIndPatronObligado(Integer indPatronObligado) {
+		this.indPatronObligado = indPatronObligado;
+	}
+	/**
+	 * @return the indPatronConstruccion
+	 */
+	public Short getIndPatronConstruccion() {
+		return indPatronConstruccion;
+	}
+	/**
+	 * @param indPatronConstruccion the indPatronConstruccion to set
+	 */
+	public void setIndPatronConstruccion(Short indPatronConstruccion) {
+		this.indPatronConstruccion = indPatronConstruccion;
+	}
+	/**
+	 * @return the indRealizoActConstruccion
+	 */
+	public Short getIndRealizoActConstruccion() {
+		return indRealizoActConstruccion;
+	}
+	/**
+	 * @param indRealizoActConstruccion the indRealizoActConstruccion to set
+	 */
+	public void setIndRealizoActConstruccion(Short indRealizoActConstruccion) {
+		this.indRealizoActConstruccion = indRealizoActConstruccion;
+	}
+	/**
+	 * @return the indPatronEmpresaValuada
+	 */
+	public Short getIndPatronEmpresaValuada() {
+		return indPatronEmpresaValuada;
+	}
+	/**
+	 * @param indPatronEmpresaValuada the indPatronEmpresaValuada to set
+	 */
+	public void setIndPatronEmpresaValuada(Short indPatronEmpresaValuada) {
+		this.indPatronEmpresaValuada = indPatronEmpresaValuada;
+	}
+	/**
+	 * @return the desNumeroFolioDictamen
+	 */
+	public String getDesNumeroFolioDictamen() {
+		return desNumeroFolioDictamen;
+	}
+	/**
+	 * @param desNumeroFolioDictamen the desNumeroFolioDictamen to set
+	 */
+	public void setDesNumeroFolioDictamen(String desNumeroFolioDictamen) {
+		this.desNumeroFolioDictamen = desNumeroFolioDictamen;
+	}
+	/**
+	 * @return the fecRegistroAlta
+	 */
+	public Date getFecRegistroAlta() {
+		return fecRegistroAlta;
+	}
+	/**
+	 * @param fecRegistroAlta the fecRegistroAlta to set
+	 */
+	public void setFecRegistroAlta(Date fecRegistroAlta) {
+		this.fecRegistroAlta = fecRegistroAlta;
+	}
+	/**
+	 * @return the fecRegistroBaja
+	 */
+	public Date getFecRegistroBaja() {
+		return fecRegistroBaja;
+	}
+	/**
+	 * @param fecRegistroBaja the fecRegistroBaja to set
+	 */
+	public void setFecRegistroBaja(Date fecRegistroBaja) {
+		this.fecRegistroBaja = fecRegistroBaja;
+	}
+	/**
+	 * @return the fecRegistroAutorizado
+	 */
+	public Date getFecRegistroAutorizado() {
+		return fecRegistroAutorizado;
+	}
+	/**
+	 * @param fecRegistroAutorizado the fecRegistroAutorizado to set
+	 */
+	public void setFecRegistroAutorizado(Date fecRegistroAutorizado) {
+		this.fecRegistroAutorizado = fecRegistroAutorizado;
+	}
+	/**
+	 * @return the cveIdUsuario
+	 */
+	public String getCveIdUsuario() {
+		return cveIdUsuario;
+	}
+	/**
+	 * @param cveIdUsuario the cveIdUsuario to set
+	 */
+	public void setCveIdUsuario(String cveIdUsuario) {
+		this.cveIdUsuario = cveIdUsuario;
+	}
+	/**
+	 * @return the cveIdTipoDictamenId
+	 */
+	public Long getCveIdTipoDictamenId() {
+		return cveIdTipoDictamenId;
+	}
+	/**
+	 * @param cveIdTipoDictamenId the cveIdTipoDictamenId to set
+	 */
+	public void setCveIdTipoDictamenId(Long cveIdTipoDictamenId) {
+		this.cveIdTipoDictamenId = cveIdTipoDictamenId;
+	}
+	/**
+	 * @return the cveIdEjerFiscalId
+	 */
+	public Long getCveIdEjerFiscalId() {
+		return cveIdEjerFiscalId;
+	}
+	/**
+	 * @param cveIdEjerFiscalId the cveIdEjerFiscalId to set
+	 */
+	public void setCveIdEjerFiscalId(Long cveIdEjerFiscalId) {
+		this.cveIdEjerFiscalId = cveIdEjerFiscalId;
+	}
+   
+
+
 
 }
--- source/dictamen/dictamen-soa/dictamen-soa-services/src/main/java/mx/gob/imss/cit/dictamen/services/transformer/TransformerServiceUtils.java
+++ source/dictamen/dictamen-soa/dictamen-soa-services/src/main/java/mx/gob/imss/cit/dictamen/services/transformer/TransformerServiceUtils.java
@@ -14,7 +14,11 @@ import mx.gob.imss.cit.dictamen.commons.to.BovedaDocumentoTO;
 import mx.gob.imss.cit.dictamen.commons.to.BovedaHeaderTO;
 import mx.gob.imss.cit.dictamen.commons.to.BovedaMetadataTO;
 import mx.gob.imss.cit.dictamen.commons.to.BovedaTramiteTO;
+import mx.gob.imss.cit.dictamen.commons.to.domain.PatronDictamenTO;
 import mx.gob.imss.cit.dictamen.commons.util.TransformerHelper;
+import mx.gob.imss.cit.dictamen.model.NdcEjercicioFiscalDO;
+import mx.gob.imss.cit.dictamen.model.NdcTipoDictamenDO;
+import mx.gob.imss.cit.dictamen.model.NdtPatronDictamenDO;
 import mx.gob.imss.cit.dictamen.services.constants.DictamenServicesConstants;
 import mx.gob.imss.cit.ws.commonschema.SGBDE;
 
@@ -32,12 +36,16 @@ public class TransformerServiceUtils {
 		mapClass.put(mx.gob.imss.cit.bp.ws.bovedapersonalcommonschema.Tramite.class,mx.gob.imss.cit.dictamen.commons.to.BovedaTramiteTO.class);
 		mapClass.put(mx.gob.imss.cit.bp.ws.bovedapersonalcommonschema.BaseObject.class,mx.gob.imss.cit.dictamen.commons.to.BovedaBaseObjectTO.class);
 		mapClass.put(mx.gob.imss.cit.ws.commonschema.SGBDE.class,mx.gob.imss.cit.dictamen.commons.to.BovedaHeaderTO.class);
+		mapClass.put(mx.gob.imss.cit.dictamen.model.NdtPatronDictamenDO.class,mx.gob.imss.cit.dictamen.commons.to.domain.PatronDictamenTO.class);
+		
+		
 		mapClass.put(mx.gob.imss.cit.dictamen.commons.to.BovedaDocumentoTO.class, mx.gob.imss.cit.bp.ws.bovedapersonalcommonschema.Document.class);
 		mapClass.put(mx.gob.imss.cit.dictamen.commons.to.BovedaActorTO.class, mx.gob.imss.cit.bp.ws.bovedapersonalcommonschema.Actor.class);
 		mapClass.put(mx.gob.imss.cit.dictamen.commons.to.BovedaMetadataTO.class, mx.gob.imss.cit.bp.ws.bovedapersonalcommonschema.Metadata.class);
 		mapClass.put(mx.gob.imss.cit.dictamen.commons.to.BovedaTramiteTO.class, mx.gob.imss.cit.bp.ws.bovedapersonalcommonschema.Tramite.class);
 		mapClass.put(mx.gob.imss.cit.dictamen.commons.to.BovedaBaseObjectTO.class, mx.gob.imss.cit.bp.ws.bovedapersonalcommonschema.BaseObject.class);
 		mapClass.put(mx.gob.imss.cit.dictamen.commons.to.BovedaHeaderTO.class, mx.gob.imss.cit.ws.commonschema.SGBDE.class);		
+		mapClass.put(mx.gob.imss.cit.dictamen.commons.to.domain.PatronDictamenTO.class,mx.gob.imss.cit.dictamen.model.NdtPatronDictamenDO.class);
 	}
 	
 	
@@ -48,62 +56,75 @@ public class TransformerServiceUtils {
 
 
 	public static BovedaDocumentoTO transformer( Document document ) {
-		BovedaDocumentoTO cont=  (BovedaDocumentoTO) TransformerHelper.get(mapClass,document, BovedaDocumentoTO.class,DictamenServicesConstants.PROFUNDIDAD_MAPEO) ;
-		return cont;
+		
+		return (BovedaDocumentoTO) TransformerHelper.get(mapClass,document, BovedaDocumentoTO.class,DictamenServicesConstants.PROFUNDIDAD_MAPEO) ;
 	}	
 	
-	public static BovedaMetadataTO transformer(Metadata metadata){
-		BovedaMetadataTO cont=  (BovedaMetadataTO) TransformerHelper.get(mapClass,metadata, BovedaMetadataTO.class,DictamenServicesConstants.PROFUNDIDAD_MAPEO) ;
-		return cont;
+	public static BovedaMetadataTO transformer(Metadata metadata){		
+		return (BovedaMetadataTO) TransformerHelper.get(mapClass,metadata, BovedaMetadataTO.class,DictamenServicesConstants.PROFUNDIDAD_MAPEO) ;
 	}
 	
 	public static BovedaTramiteTO transformer( Tramite tramite ) {
-		BovedaTramiteTO cont=  (BovedaTramiteTO) TransformerHelper.get(mapClass,tramite, BovedaTramiteTO.class,DictamenServicesConstants.PROFUNDIDAD_MAPEO) ;
-		return cont;
+		return (BovedaTramiteTO) TransformerHelper.get(mapClass,tramite, BovedaTramiteTO.class,DictamenServicesConstants.PROFUNDIDAD_MAPEO);
 	}	
 	
 	public static BovedaActorTO transformer(Actor actor){
-		BovedaActorTO cont=  (BovedaActorTO) TransformerHelper.get(mapClass,actor, BovedaActorTO.class,DictamenServicesConstants.PROFUNDIDAD_MAPEO) ;
-		return cont;
+		return  (BovedaActorTO) TransformerHelper.get(mapClass,actor, BovedaActorTO.class,DictamenServicesConstants.PROFUNDIDAD_MAPEO);
 	}
 	
 	public static BovedaBaseObjectTO transformer(BaseObject baseObject){
-		BovedaBaseObjectTO cont=  (BovedaBaseObjectTO) TransformerHelper.get(mapClass,baseObject, BovedaBaseObjectTO.class,DictamenServicesConstants.PROFUNDIDAD_MAPEO) ;
-		return cont;
+		return (BovedaBaseObjectTO) TransformerHelper.get(mapClass,baseObject, BovedaBaseObjectTO.class,DictamenServicesConstants.PROFUNDIDAD_MAPEO);
 	}	
 	
-	public static BovedaHeaderTO transformer(SGBDE sgbde){
-		BovedaHeaderTO cont=  (BovedaHeaderTO) TransformerHelper.get(mapClass,sgbde, BovedaHeaderTO.class,DictamenServicesConstants.PROFUNDIDAD_MAPEO) ;
-		return cont;
+	public static BovedaHeaderTO transformer(SGBDE sgbde){		
+		return (BovedaHeaderTO) TransformerHelper.get(mapClass,sgbde, BovedaHeaderTO.class,DictamenServicesConstants.PROFUNDIDAD_MAPEO);
 	}	
 
-	public static Document transformer( BovedaDocumentoTO documentoTO ) {
-		Document cont=  (Document) TransformerHelper.get(mapClass,documentoTO, Document.class,DictamenServicesConstants.PROFUNDIDAD_MAPEO) ;
-		return cont;
+	public static Document transformer( BovedaDocumentoTO documentoTO ) {		
+		return (Document) TransformerHelper.get(mapClass,documentoTO, Document.class,DictamenServicesConstants.PROFUNDIDAD_MAPEO);
 	}	
 	
-	public static Metadata transformer(BovedaMetadataTO metadataTO){
-		Metadata cont=  (Metadata) TransformerHelper.get(mapClass,metadataTO, Metadata.class,DictamenServicesConstants.PROFUNDIDAD_MAPEO) ;
-		return cont;
+	public static Metadata transformer(BovedaMetadataTO metadataTO){		
+		return (Metadata) TransformerHelper.get(mapClass,metadataTO, Metadata.class,DictamenServicesConstants.PROFUNDIDAD_MAPEO);
 	}
 	
 	public static Tramite transformer( BovedaTramiteTO tramiteTO ) {
-		Tramite cont=  (Tramite) TransformerHelper.get(mapClass,tramiteTO, Tramite.class,DictamenServicesConstants.PROFUNDIDAD_MAPEO) ;
-		return cont;
+		return  (Tramite) TransformerHelper.get(mapClass,tramiteTO, Tramite.class,DictamenServicesConstants.PROFUNDIDAD_MAPEO);
 	}	
 	
-	public static Actor transformer(BovedaActorTO actorTO){
-		Actor cont=  (Actor) TransformerHelper.get(mapClass,actorTO, Actor.class,DictamenServicesConstants.PROFUNDIDAD_MAPEO) ;
-		return cont;
+	public static Actor transformer(BovedaActorTO actorTO){		
+		return (Actor) TransformerHelper.get(mapClass,actorTO, Actor.class,DictamenServicesConstants.PROFUNDIDAD_MAPEO);
 	}
 	
 	public static BaseObject transformer(BovedaBaseObjectTO baseObjectTO){
-		BaseObject cont=  (BaseObject) TransformerHelper.get(mapClass,baseObjectTO, BaseObject.class,DictamenServicesConstants.PROFUNDIDAD_MAPEO) ;
+		return (BaseObject) TransformerHelper.get(mapClass,baseObjectTO, BaseObject.class,DictamenServicesConstants.PROFUNDIDAD_MAPEO) ;
+	}	
+	
+	public static SGBDE transformer(BovedaHeaderTO headerTO){ 
+		return (SGBDE) TransformerHelper.get(mapClass,headerTO, SGBDE.class,DictamenServicesConstants.PROFUNDIDAD_MAPEO) ;
+	}
+	
+	public static NdtPatronDictamenDO transformer( PatronDictamenTO patron ) {
+		NdtPatronDictamenDO cont=  (NdtPatronDictamenDO) TransformerHelper.get(mapClass,patron, NdtPatronDictamenDO.class,DictamenServicesConstants.PROFUNDIDAD_MAPEO) ;
+		NdcTipoDictamenDO ndcTipoDictamenDO=new NdcTipoDictamenDO();
+		ndcTipoDictamenDO.setCveIdTipoDictamen(patron.getCveIdTipoDictamenId());
+		NdcEjercicioFiscalDO ndcEjercicioFiscalDO=new NdcEjercicioFiscalDO();
+		ndcEjercicioFiscalDO.setCveIdEjerFiscal(patron.getCveIdEjerFiscalId());
+		
 		return cont;
 	}	
 	
-	public static SGBDE transformer(BovedaHeaderTO headerTO){
-		SGBDE cont=  (SGBDE) TransformerHelper.get(mapClass,headerTO, SGBDE.class,DictamenServicesConstants.PROFUNDIDAD_MAPEO) ;
+	public static PatronDictamenTO transformer( NdtPatronDictamenDO patron ) {
+		PatronDictamenTO cont=  (PatronDictamenTO) TransformerHelper.get(mapClass,patron, PatronDictamenTO.class,DictamenServicesConstants.PROFUNDIDAD_MAPEO) ;
+		
+		if(patron.getCveIdTipoDictamen()!=null){
+			cont.setCveIdTipoDictamenId(patron.getCveIdTipoDictamen().getCveIdTipoDictamen());
+		}
+		if(patron.getCveIdEjerFiscal()!=null){
+			cont.setCveIdEjerFiscalId(patron.getCveIdEjerFiscal().getCveIdEjerFiscal());
+		}
+
 		return cont;
-	}
+	}	
+	
 }
