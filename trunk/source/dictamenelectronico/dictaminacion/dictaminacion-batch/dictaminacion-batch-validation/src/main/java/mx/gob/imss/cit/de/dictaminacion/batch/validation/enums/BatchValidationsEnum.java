2eb62ced4481841d2c646fdbea83301134d5a65b
alejandro.fuentes <alejandro.fuentes@6f5771a2-32be-460e-a771-7d6402c15993>
2015-10-17 02:04:41 +0000

    sse sube refactor de paquetes

---
+++ trunk/source/dictamenelectronico/dictaminacion/dictaminacion-batch/dictaminacion-batch-validation/src/main/java/mx/gob/imss/cit/de/dictaminacion/batch/validation/enums/BatchValidationsEnum.java
@@ -0,0 +1,30 @@
+package mx.gob.imss.cit.de.dictaminacion.batch.validation.enums;
+
+/**
+ * 
+ * 
+ * @author cmarmolejo
+ * 
+ */
+public enum BatchValidationsEnum {
+	OBLIGATORIO("1"),
+	LONGITUD_10_11("2"),
+	LONGITUD_11("3"),
+	LONGITUD_13("4"),
+	ALFANUMERICO_SIN_ESPACIOS_NI_CARACTERES_ESPECIALES("[A-Za-z0-9]+"),
+	ALFANUMERICO_CON_ACENTO_Y_ENIE("[A-Z,ÑÁÉÍÓÚ]+"),
+	NUMERICO("[0-9]+"),
+	RFC("[A-Za-z,Ññ,&]{3,4}[0-9]{2}[0-1][0-9][0-3][0-9][A-Za-z,0-9]?[A-Za-z,0-9]?[0-9,A-Za-z]?");
+	
+	private String regExp;
+
+	private BatchValidationsEnum(String regExp) {
+		this.regExp = regExp;
+	}
+
+	public String getRegExp() {
+		return regExp;
+	}
+	
+}
+
