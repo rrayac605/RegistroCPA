7c29e382419464ba0cd5885d304799c5f559131a
alejandro.fuentes <alejandro.fuentes@6f5771a2-32be-460e-a771-7d6402c15993>
2015-10-17 01:44:37 +0000

    version inicial  proyecto integrado

---
+++ source/dictamenelectronico/dictaminacion/dictaminacion-batch/batch-persistence/src/main/java/mx/gob/imss/cit/dictamen/batch/persistence/to/TxtFileTO.java
@@ -0,0 +1,20 @@
+package mx.gob.imss.cit.dictamen.batch.persistence.to;
+
+/**
+ * 
+ * 
+ * @author cmarmolejo
+ * 
+ */
+public class TxtFileTO {
+	private String line;
+
+	public String getLine() {
+		return line;
+	}
+
+	public void setLine(String line) {
+		this.line = line;
+	}	
+}
+
