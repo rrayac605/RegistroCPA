dca5dc4665285b3b3cba960b62b6a9d8d36f8da3
alejandro.fuentes <alejandro.fuentes@6f5771a2-32be-460e-a771-7d6402c15993>
2015-10-17 01:44:37 +0000

    version inicial  proyecto integrado

---
+++ trunk/source/dictamenelectronico/dictaminacion/dictaminacion-batch/batch-validation/src/main/java/mx/gob/imss/cit/dictamen/batch/validation/processors/TxtFileProcessor.java
@@ -0,0 +1,28 @@
+package mx.gob.imss.cit.dictamen.batch.validation.processors;
+
+import org.springframework.batch.item.ItemProcessor;
+
+import mx.gob.imss.cit.dictamen.batch.validation.enums.BatchValidationsEnum;
+import mx.gob.imss.cit.dictamen.batch.validation.to.A1TO;
+import mx.gob.imss.cit.dictamen.batch.validation.util.Validator;
+
+/**
+ * 
+ * 
+ * @author cmarmolejo
+ * 
+ */
+public class TxtFileProcessor implements ItemProcessor<A1TO, A1TO> {
+
+	@Override
+	public A1TO process(A1TO paramI) throws Exception {
+        if(Validator.validate(BatchValidationsEnum.OBLIGATORIO, paramI.getRegPatronal())){
+            
+        }else{
+            
+        } 
+		return paramI;
+	}
+
+}
+
