99d78c5a74a7536a5ebc209500d8810466b9f5a0
alejandro.fuentes <alejandro.fuentes@6f5771a2-32be-460e-a771-7d6402c15993>
2015-10-17 01:48:56 +0000

    version inicial  proyecto integrado

---
+++ trunk/source/dictamenelectronico/dictaminacion/dictaminacion-batch/dictaminacion-batch-validation/src/main/java/mx/gob/imss/cit/dictamen/batch/validation/util/Validator.java
@@ -0,0 +1,62 @@
+package mx.gob.imss.cit.dictamen.batch.validation.util;
+
+import java.util.regex.Pattern;
+
+import mx.gob.imss.cit.dictamen.batch.validation.enums.BatchValidationsEnum;
+
+/**
+ * 
+ * 
+ * @author cmarmolejo
+ * 
+ */
+public class Validator {
+	
+	private Validator(){
+		
+	}
+	
+	public static boolean validate(BatchValidationsEnum tipoValidacion, String cadenaAEvaluar){		
+		switch(tipoValidacion){
+		case ALFANUMERICO_SIN_ESPACIOS_NI_CARACTERES_ESPECIALES:
+			return Pattern.matches(BatchValidationsEnum.ALFANUMERICO_SIN_ESPACIOS_NI_CARACTERES_ESPECIALES.getRegExp(), cadenaAEvaluar);
+		case ALFANUMERICO_CON_ACENTO_Y_ENIE:
+			return Pattern.matches(BatchValidationsEnum.ALFANUMERICO_CON_ACENTO_Y_ENIE.getRegExp(), cadenaAEvaluar);
+		case LONGITUD_10_11:
+			return (validateLengt(10, cadenaAEvaluar) || validateLengt(11, cadenaAEvaluar) ? true:false);
+		case LONGITUD_11:
+			return validateLengt(11, cadenaAEvaluar);
+		case LONGITUD_13:
+			return validateLengt(13, cadenaAEvaluar);
+		case NUMERICO:
+			return Pattern.matches(BatchValidationsEnum.NUMERICO.getRegExp(), cadenaAEvaluar);
+		case OBLIGATORIO:
+			return isNotEmpty(cadenaAEvaluar);
+		case RFC:
+			return Pattern.matches(BatchValidationsEnum.RFC.getRegExp(), cadenaAEvaluar);
+		default:
+			return false;
+		}			
+	}
+	
+	public static boolean isNotEmpty(String cadenaAEvaluar){
+		if(cadenaAEvaluar == null){
+			return false;
+		}else if(cadenaAEvaluar.isEmpty()){
+			return false;
+		}else if(cadenaAEvaluar.trim().isEmpty()){
+			return false;
+		}else{
+			return true;
+		}
+	}
+	
+	public static boolean validateLengt(int length, String cadenaAEvaluar){
+		if(cadenaAEvaluar.length() == length){
+			return true;
+		}else{
+			return false;
+		}		
+	}
+}
+
