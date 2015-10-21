8f64f29c7f00fd6b7f747bf9d210cf9cb469fd4f
alejandro.fuentes <alejandro.fuentes@6f5771a2-32be-460e-a771-7d6402c15993>
2015-10-21 17:42:45 +0000

    cambios de registros patronales

--- trunk/source/dictamenelectronico/dictaminacion/dictaminacion-web/src/main/java/mx/gob/imss/cit/de/dictaminacion/web/beans/CargaRegistroPatronalBean.java
+++ trunk/source/dictamenelectronico/dictaminacion/dictaminacion-web/src/main/java/mx/gob/imss/cit/de/dictaminacion/web/beans/CargaRegistroPatronalBean.java
@@ -1,14 +1,24 @@
 package mx.gob.imss.cit.de.dictaminacion.web.beans;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.portlet.PortletContext;
 
 import org.apache.log4j.Logger;
-import org.primefaces.event.FileUploadEvent;
+import org.primefaces.context.RequestContext;
 
+import mx.gob.imss.cit.de.dictaminacion.integration.api.dto.domain.PatronAsociadoDTO;
 import mx.gob.imss.cit.de.dictaminacion.web.beans.base.BaseBean;
+import mx.gob.imss.cit.de.dictaminacion.web.enums.MensajesNotificacionesEnum;
 import mx.gob.imss.cit.de.dictaminacion.web.pages.CargaRegistroPatronalPage;
+import mx.gob.imss.cit.de.dictaminacion.web.util.FacesUtils;
 
 @ManagedBean(name = "cargaRegistroPatronalBean")
 @ViewScoped
@@ -24,17 +34,41 @@ public class CargaRegistroPatronalBean extends BaseBean {
 	
 
 
-	public void init(){
-	
+	public String init(){
+		 List<PatronAsociadoDTO> lista=new ArrayList<PatronAsociadoDTO>();		 
+		cargaRegistroPatronalPage.setListaRegistrosPatronales(lista);
+		
+		return "";
+		
 	}
 	
-	 public void upload(FileUploadEvent event) {
-		 byte[] archivoBytes=null;
-		 cargaRegistroPatronalPage.setFile(event.getFile());
-          archivoBytes=cargaRegistroPatronalPage.getFile().getContents();
+	 public void cargarRegistrosPatronales() {
+		 System.out.println("entro bean");
 
-	        
-	        
+		 List<PatronAsociadoDTO> lista=new ArrayList<PatronAsociadoDTO>();
+		 FacesContext ctx = FacesContext.getCurrentInstance();
+		 PortletContext request = (PortletContext) ctx.getExternalContext().getContext();
+		 System.out.println(request.getRealPath("/"));
+		
+		 try {
+			 File file= new File(request.getRealPath("/")+"/registroPatronal.txt");
+			 Scanner sc=new Scanner(file);
+			 while(sc.hasNext()){
+				 PatronAsociadoDTO patron=new PatronAsociadoDTO();
+				 patron.setDesRfcPatron( sc.nextLine());
+				 System.out.println(patron.getDesRfcPatron());
+				 lista.add(patron);
+			 }
+			 cargaRegistroPatronalPage.setListaRegistrosPatronales(lista);
+
+		     FacesUtils.messageSuccess(MensajesNotificacionesEnum.MSG_EXITO_REGISTROS_PATRONALES.getCode());
+		    
+		} catch (Exception e) {
+			FacesUtils.messageError(MensajesNotificacionesEnum.MSG_ERROR_REGISTROS_PATRONALES.getCode());
+			e.printStackTrace();
+		}
+		
+
 	    }
 
 	
