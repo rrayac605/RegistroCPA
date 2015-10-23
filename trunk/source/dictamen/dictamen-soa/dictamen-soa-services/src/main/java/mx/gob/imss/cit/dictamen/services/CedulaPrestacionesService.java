1f7734a27cf080a090ebb9fc493bec0fbc8812f1
alejandro.fuentes <alejandro.fuentes@6f5771a2-32be-460e-a771-7d6402c15993>
2015-10-23 17:41:04 +0000

    se elimina proyecto dictamen

--- trunk/source/dictamen/dictamen-soa/dictamen-soa-services/src/main/java/mx/gob/imss/cit/dictamen/services/CedulaPrestacionesService.java
+++
@@ -1,8 +0,0 @@
-package mx.gob.imss.cit.dictamen.services;
-
-import mx.gob.imss.cit.dictamen.commons.exception.DictamenException;
-import mx.gob.imss.cit.dictamen.commons.to.domain.CedulaPrestacionesTO;
-
-public interface CedulaPrestacionesService {
-	CedulaPrestacionesTO saveCedulaPrestaciones() throws DictamenException;
-}
