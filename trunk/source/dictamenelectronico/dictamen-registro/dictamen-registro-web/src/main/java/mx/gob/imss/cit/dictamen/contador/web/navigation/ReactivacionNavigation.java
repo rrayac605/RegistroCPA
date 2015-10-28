13531e1d613de618985b6d88b5b084eaf147f434
osanchez <osanchez@6f5771a2-32be-460e-a771-7d6402c15993>
2015-10-28 04:11:34 +0000

    Navegacion de acreditacion

---
+++ trunk/source/dictamenelectronico/dictamen-registro/dictamen-registro-web/src/main/java/mx/gob/imss/cit/dictamen/contador/web/navigation/ReactivacionNavigation.java
@@ -0,0 +1,12 @@
+package mx.gob.imss.cit.dictamen.contador.web.navigation;
+
+public final class ReactivacionNavigation {
+
+	   public static final String REACTIVACION_EMPTY="";
+	   public static final String REACTIVACION_SOLICITUD="reactivacion_solicitud";
+	   public static final String REACTIVACION_CONTADOR="reactivacion_contador";
+	   public static final String REACTIVACION_DESPACHO="reactivacion_despacho";
+	   public static final String REACTIVACION_COLEGIO="reactivacion_colegio";
+	   public static final String REACTIVACION_DOCUMENTACION="reactivacion_documentacion";
+
+}
