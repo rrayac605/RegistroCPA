5113d5db0d06f6aaf2cc0ef2ed2f8e6116076859
osanchez <osanchez@6f5771a2-32be-460e-a771-7d6402c15993>
2015-10-28 04:11:34 +0000

    Navegacion de acreditacion

--- source/dictamenelectronico/dictamen-registro/dictamen-registro-web/src/main/java/mx/gob/imss/cit/dictamen/contador/web/navigation/ActivacionNavigation.java
+++ source/dictamenelectronico/dictamen-registro/dictamen-registro-web/src/main/java/mx/gob/imss/cit/dictamen/contador/web/navigation/ActivacionNavigation.java
@@ -2,7 +2,7 @@ package mx.gob.imss.cit.dictamen.contador.web.navigation;
 
 public final class ActivacionNavigation {
 	
-  public static final String ACTIVACION_EMPTY="";
+   public static final String ACTIVACION_EMPTY="";
    public static final String ACTIVACION_SOLICITUD="activacion_solicitud";
    public static final String ACTIVACION_CONTADOR="activacion_contador";
    public static final String ACTIVACION_DESPACHO="activacion_despacho";
